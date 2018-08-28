249b6c50ad4d4595446a6182022d0b0aa125627c
eltons <elton@canway.net>
2018-08-28 21:08:19 +0800

    Initial commit

---
+++ src/main/java/java8Test1.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author eltons,  Date on 2018-08-28.
+ */
+public class java8Test1 {
+
+
+    @Test
+    public void arraysTest() {
+        List<String> strings = Arrays.asList("a", "b", "c");
+
+        Comparator<Integer> comparator = (a, b) -> Integer.compare(a, b);
+
+        strings.forEach(System.out::print);
+
+        strings.stream().filter(it -> it.length() > 2)
+                .map(String::getBytes)
+                .forEach(System.out::println);
+
+
+    }
+}
